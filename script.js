dedc49bd88e1938af06c0d4ff4b384c27a69e779
adarshtsatheesh <adarshtsatheesh@gmail.com>
2024-03-27 00:47:13 +0530

    cursor and about

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ document.addEventListener('DOMContentLoaded', function(){
   }
 });
 
+
+// Cursor
+const cursorSmall = document.querySelector('.small');
+const cursorBig = document.querySelector('.big');
+
+
+const positionElement = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+   
+  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  
+  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+}
+
+window.addEventListener('mousemove', positionElement)

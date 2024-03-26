24d906f4e8a5f2b24403c44adb9a391fbedae4a4
adarshtsatheesh <adarshtsatheesh@gmail.com>
2024-03-26 21:31:35 +0530

    99% perfect hero & navbar

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Animated Text Hero Section
 document.addEventListener('DOMContentLoaded', function(){
-    const typed = document.querySelector('.typed');
-    if (typed) {
-      let typed_strings = typed.getAttribute('data-typed-items');
-      typed_strings = typed_strings.split(',');
-      new Typed('.typed', {
-        strings: typed_strings,
-        loop: true,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 2000
-      });
-    }
-  });
-  
+  const typed = document.querySelector('.typed');
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+});
+
